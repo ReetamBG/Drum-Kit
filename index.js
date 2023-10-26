d8f8d5dec0d94304d147bc378f4c4479e9d9e122
ReetamBG <r33t4m@gmail.com>
2023-10-27 00:19:28 +0530

    Finalisation

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const button = document.querySelectorAll(".drum");
 
 for(let i = 0; i < button.length; i++){
-    button[i].addEventListener("click", clickToPlay);
+    button[i].addEventListener("click", clickToPlay);      // To look for clicks in the .drum selector elements
 }
+
+document.addEventListener("keydown", pressToPlay);     // To look for keypresses in the entire document (i.e when we are on the page)
 
 function clickToPlay(){
     let buttonInnerHTML = this.innerHTML;
     playSound(buttonInnerHTML);
 }
-
-document.addEventListener("keydown", pressToPlay);
 
 function pressToPlay(event){            // The event (not a keyword) here is used to obtain info about the keypress
     playSound(event.key);               // event.key gives us the key letter which was pressed
